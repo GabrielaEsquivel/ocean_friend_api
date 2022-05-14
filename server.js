f2d59f4b73ddf1ec99eaca8645f67a0bdce058fb
Andrea <andrea.rueda-ext@dominion-global.com>
2022-05-14 14:26:31 -0500

    terminando servicios de Alarma

--- server.js
+++ server.js
@@ -3,14 +3,52 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
-
-// Require para usar Prisma
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const AlarmController = require('./app/controllers/AlarmController');
 
 app.get('/', (req, res) => {
-  res.json({message: 'alive'});
+  res.json({message: 'Server on!'});
 });
+/***  Alarms  */
+app.get('/alarms', async (req, res) => {
+  const allAlarms = await AlarmController.getAllAlarms();
+  res.json(allAlarms);
+});
+
+app.get('/alarms/last', async (req, res) => {
+  const lastAlarms = await AlarmController.getTheLastAlarms();
+  res.json(lastAlarms);
+});
+
+app.post('/alarms/create', async (req, res) => {
+  const alarm = req.body;
+  const alarmCreated = await AlarmController.create(alarm)
+  return res.json({  alarmCreated });
+});
+
+app.put('/alarms/seen/:id', async (req, res) => {
+  //const id = parseInt(req.params.id);
+  const alarm = {id: parseInt(req.params.id), isSeen: req.body.isSeen }
+  const updated = await AlarmController.editSeen(alarm)
+  return res.json(updated);
+});
+
+app.put('/alarms/attend/:id', async (req, res) => {
+  const alarm = {id: parseInt(req.params.id), isAttended: req.body.isAttended }
+  const updated = await AlarmController.editAttended(alarm)
+  return res.json(updated);
+});
+
+app.delete('/alarms/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const deleted = await AlarmController.delete(id)
+  return res.json(deleted);
+});
+/*app.get('/explorers/:id', async (req, res) => {
+  const id = req.params.id;
+  const explorer = await prisma.explorer.findUnique({ where: { id: parseInt(id) } });
+  res.json(explorer);
+});*/
+
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
