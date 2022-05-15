9e4131c18c34d351f64e8908063d7c6024208dc1
Andrea <andrea.rueda-ext@dominion-global.com>
2022-05-15 00:25:45 -0500

    Agregando bcrypt y UserService

---
+++ app/services/UserService.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcrypt')
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+class UserService {
+    static encryptPassword(password) {
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        return bcrypt.hashSync(password, salt);
+    }
+    static async create(nwUser) {
+        const password = this.encryptPassword(nwUser.passwd)
+        nwUser.passwd = password;
+        const create = await prisma.user.create({ data: nwUser });
+        return { user: create, created: true };
+    }
+    static async getAll() {
+        const users = await prisma.user.findMany({});
+        return users;
+    }
+    static async update(user) {
+        const updated = await prisma.user.update({
+            where: {
+                id: user.id
+            },
+            data: {
+                username: user.username,
+                passwd: user.passwd
+            }
+        })
+        return { data: updated, updated: true };
+    }
+
+    static async changePassword(nwPsswd) {
+        const password = this.encryptPassword(nwUser.passwd)
+        nwPsswd.passwd = password;
+        const updated = await prisma.user.update({
+            where: {
+                id: nwPsswd.id
+            },
+            data: {
+                passwd: nwPsswd.passwd
+            }
+        })
+        return { data: updated };
+    }
+
+    static async delete(idUser) {
+        const deleted = await prisma.user.delete({ where: { id: idUser } });
+        return deleted ? true : false;
+    }
+}
+
+module.exports = UserService;
